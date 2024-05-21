ca6e4920549815cad81d85004390a174e8274ee1
SebastianEugenioP <sebastianeugeniopinzon@gmail.com>
2024-05-20 21:28:24 -0500

    Add files via upload
    Primera versión de control de motores

---
+++ motores.ino
@@ -0,0 +1,34 @@
+void MOTOR1 (int PWM,bool IN1,bool IN2);
+void MOTOR2 (int PWM,bool IN3,bool IN4);
+
+
+
+void setup() {
+pinMode(2,OUTPUT);//IN1
+pinMode(3,OUTPUT);//PWM MOTOR 1
+pinMode(4,OUTPUT);//IN2
+pinMode(5,OUTPUT);//PWM MOTOR 2
+pinMode(6,OUTPUT);//IN3
+pinMode(7,OUTPUT);//IN4
+
+}
+
+void loop() {
+
+ 
+
+}
+void MOTOR1 (int PWM,bool IN1,bool IN2)
+{
+digitalWrite(2,IN1);
+digitalWrite(4,IN2);
+analogWrite(3,PWM);
+}
+
+void MOTOR2 (int PWM,bool IN3,bool IN4)
+{
+digitalWrite(6,IN3);
+digitalWrite(7,IN4);
+analogWrite(5,PWM);
+}
+
