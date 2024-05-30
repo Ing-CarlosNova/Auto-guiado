16f0082aa2f4a3bf9168dcd5baef2f1e7716907b
Carlos Nova <carlos.nova.ieu@gmail.com>
2024-05-29 22:41:44 -0500

    Update Sensor_de_linea_negra.ino

--- Sensor_de_linea_negra.ino
+++ Sensor_de_linea_negra.ino
@@ -1,38 +1,65 @@
 int cont=0;
-int s1,s2,s3,s4 = 0;
+int s1=10,s2=11,s3=12,s4=13;
+int v, v1, v2, v3;
 
 //,s2,s3,s4;//Variables de los sensores
 void setup() {
+  
   pinMode(9,OUTPUT);//IN1
   pinMode(3,OUTPUT);//PWM MOTOR 1
   pinMode(4,OUTPUT);//IN2
   pinMode(5,OUTPUT);//PWM MOTOR 2
   pinMode(6,OUTPUT);//IN3
   pinMode(7,OUTPUT);//IN4
-  pinMode(10,INPUT);//Sensor Derecha
-  pinMode(11,INPUT);//Sensor Centro Derecha
-  pinMode(12,INPUT);//Sensor Centro Izquierda 
-  pinMode(13,INPUT);//Sensor Izquierda
+  pinMode(s2,INPUT);
+  pinMode(s2,INPUT);
+  pinMode(s3,INPUT);//Sensor Derecha
+  pinMode(s4,INPUT);
+
   Serial.begin(9600); 
 }
 
 void loop() {
-s1=digitalRead(10);//Derecha
-s2=digitalRead(11);//Centro Derecha
-s3=digitalRead(12);//Centro Izquierda
-s4=digitalRead(13);//Izquierda
-//Impresión de Estados de los Sensores 
-Serial.println(s1);
-Serial.println(s2);
-Serial.println(s3);
-Serial.println(s4);
-Serial.println("____");
-SEGUIDOR(s1,s2,s3,s4);//Función que controla los motores con los estados.
+  v=digitalRead(s1);//Derecha
+  v1=digitalRead(s2);//Derecha
+  v2=digitalRead(s3);//Derecha
+  v3=digitalRead(s4);//Derecha
+
+   Serial.println(v);
+   Serial.println(v1);
+   Serial.println(v2);
+   Serial.println(v3);
+   Serial.println("__");
+
+  SEGUIDOR(v,v1,v2,v3);
 
 }
 
-//Funciones de Control de Motores
-//Los parametros son los terminales del motor y la velocidad
+void SEGUIDOR(int st1, int st2, int st3, int st4)
+{
+  if((st1==0)&&(st4==1))//Los sensores del medio detectan el color negro.
+    {
+    MOTOR1(150,1,0);
+    MOTOR2(150,0,0);
+    delay(20);
+    }
+      else if((st1==1)&&(st4==0))//Los sensores del medio detectan el color negro.
+      {
+        MOTOR1(150,0,0);
+        MOTOR2(150,1,0);
+        delay(20);
+      }
+      else //Los sensores del medio detectan el color negro.
+      {
+        MOTOR1(150,1,0);
+        MOTOR2(150,1,0);
+        delay(20);
+
+      }
+      
+ 
+}
+
 void MOTOR1 (int PWM,bool IN1,bool IN2)
 {
 digitalWrite(9,IN1);
@@ -46,33 +73,4 @@ digitalWrite(6,IN3);
 digitalWrite(7,IN4);
 analogWrite(5,PWM);
 }
-//
-void SEGUIDOR(int st1,int st2,int st3,int st4)
-{
 
-  if((st2==1)&&(st3==1))//Los sensores detectan negro avanza
-    {
-      //Robot en Linea Recta
-      MOTOR1(180,0,1);
-      MOTOR2(180,0,1);
-      delay(300);
-     
-    }
-  
-  if((st1==0)&&(st4==1))//Curva a la izquierda
-    {
-     //Corrección de Giro a la Derecha
-     MOTOR1(180,0,1);
-   	 MOTOR2(180,0,0);
-     delay(300);
-     
-    }
-  if((st1==1)&&(st4==0))//Curva a la izquierda
-    {
-     //Corrección de Giro a la Derecha
-     MOTOR1(180,0,0);
-   	 MOTOR2(180,0,1);
-     delay(300);
-     
-    }  	   
-}
